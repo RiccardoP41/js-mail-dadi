fb558d040a2fdd35d3343ad7b63fdafb65681917
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-09 14:22:04 +0200

    ok

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,15 +4,38 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
+// var n1 = Math.floor(Math.random()*6) +1;
+// var n2 = Math.floor(Math.random()*6) +1;
+// var risultato;
+// document.getElementById('lancio-p1').innerHTML= "Hai fatto " + n1
+// document.getElementById('lancio-ai').innerHTML= "L\'AI ha fatto " + n2
+//
+// if (n1>n2) {
+//     risultato = "Vittoria!"
+// } else if (n2>n1) {
+//     risultato = "Sconfitta!"
+// } else {
+//     risultato = "Pareggio"
+// }
+//
+// document.getElementById('risultato').innerHTML= risultato;
+
+
+
+// *****CON DUE DADI*****
 var n1 = Math.floor(Math.random()*6) +1;
 var n2 = Math.floor(Math.random()*6) +1;
+var n3 = Math.floor(Math.random()*6) +1;
+var n4 = Math.floor(Math.random()*6) +1;
+var player = n1 + n2;
+var ai = n3 + n4;
 var risultato;
-document.getElementById('lancio-p1').innerHTML= "Hai fatto " + n1
-document.getElementById('lancio-ai').innerHTML= "L\'AI ha fatto " + n2
+document.getElementById('lancio-p1').innerHTML= "Hai fatto " + player
+document.getElementById('lancio-ai').innerHTML= "L\'AI ha fatto " + ai
 
-if (n1>n2) {
+if (player>ai) {
     risultato = "Vittoria!"
-} else if (n2>n1) {
+} else if (ai>player) {
     risultato = "Sconfitta!"
 } else {
     risultato = "Pareggio"
